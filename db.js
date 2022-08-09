1b31532d9a5dae6c8cb15250b10a982bbdd9ded6
AnikeyW <egor4ik048@gmail.com>
2022-08-09 14:29:03 +0300

    поменял host конекта к бд

--- db.js
+++ db.js
@@ -2,7 +2,8 @@ const Pool = require('pg').Pool
 const pool = new Pool({
    user: "postgres",
    password: "postgres",
-   host: "localhost",
+   // host: "localhost",
+   host: "45.147.179.186",
    port: 5432,
    database: "deps_gbdd"
 })
